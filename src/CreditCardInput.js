18b138571283c16a3349ccee3d7048d83293bc4d
Gyo Hamamoto <gyo@linksports.co.jp>
2017-11-15 20:22:03 +0900

    View.propTypes -> ViewPropTypes

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -49,7 +49,7 @@ export default class CreditCardInput extends Component {
 
     labelStyle: Text.propTypes.style,
     inputStyle: Text.propTypes.style,
-    inputContainerStyle: View.propTypes.style,
+    inputContainerStyle: ViewPropTypes.style,
 
     validColor: PropTypes.string,
     invalidColor: PropTypes.string,
